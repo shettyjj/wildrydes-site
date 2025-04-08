f40b9fce37dbf21137c558351dea8aae68af7daf
shettyjj <152796044+shettyjj@users.noreply.github.com>
2025-04-08 23:06:40 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_auwNAG9PG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '60r7lubi1kc5gnijbufp9al3g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
