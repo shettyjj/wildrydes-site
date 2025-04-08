9699e157240e35542fb627b9631257a8cab4dcd4
shettyjj <152796044+shettyjj@users.noreply.github.com>
2025-04-09 00:05:05 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://73tg6a1s0k.execute-api.eu-north-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://73tg6a1s0k.execute-api.eu-north-1.amazonaws.com/dev1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
